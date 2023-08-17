1d2b71851f13e818beada71bd5d94cc3a1ba38f4
Vasco Conde <vasconde@gmail.com>
2023-08-17 01:00:40 +0100

    feat: create spring boot package

---
+++ packages/backend/src/main/java/com/vasconde/miracleLogger/MiracleLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.vasconde.miracleLogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiracleLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiracleLoggerApplication.class, args);
+	}
+
+}
---
+++ packages/backend/src/test/java/com/vasconde/miracleLogger/MiracleLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasconde.miracleLogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiracleLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
